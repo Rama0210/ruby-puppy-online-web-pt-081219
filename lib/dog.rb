4aada4c9ffccfb1c026fb2b25f3a140acc2c3788
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-09-15 22:35:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,35 @@
-# Add your code here
 class Dog 
-  @@all
-  @@names
-end
-
+  @@all = []
+  @@names = []
+  
    attr_accessor :name
+
   def initialize(name)
-@name = name
-@@name << self.name
-self.save
+    @name = name
+    @@names << self.name
+    self.save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    names_list = [ ]
+    @@names.each do |thing|
+      if names_list.include?(thing) == false
+        names_list << "#{thing}"
+      end
+    end
+  puts names_list
 end
 
-def save
-  @@all << self
-end
+  def self.clear_all
+    @@all.clear
+    @@names.clear
+  end
+end 
